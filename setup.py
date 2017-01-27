5a74a2fb8c5ceed9164147c0cef03bed4232fcfc
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-01-27 11:46:36 -0500

    Update setup.py to reflect python 3 compatibility

--- setup.py
+++ setup.py
@@ -19,7 +19,12 @@ CLASSIFIERS = [
     'Natural Language :: English',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
