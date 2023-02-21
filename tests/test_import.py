65c33e28deb3cbacfe15a5717adcda197ea26137
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2023-02-21 10:44:27 +0000

    Add GH workflow and post-build imports for tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,4 +2,16 @@ import pytest
 
 
 def test_import():
-    from mapreader import loader
+    # This is based on all the imports found in the various tutorial notebooks
+    from mapreader import (
+        TileServer,
+        classifier,
+        load_patches,
+        loadAnnotations,
+        loader,
+        mapImages,
+        patchTorchDataset,
+        read,
+        read_patches,
+    )
+    from mapreader.annotate.utils import prepare_annotation, save_annotation
