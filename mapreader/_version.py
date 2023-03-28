c2c4a3e1209409eb8a40fa657de29043af932368
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2023-03-28 18:09:08 +0100

    Fix version number resolution

--- mapreader/_version.py
+++ mapreader/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "mapreader-"
     cfg.versionfile_source = "mapreader/_version.py"
     cfg.verbose = False
