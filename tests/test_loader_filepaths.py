3c3d24a1d4ce5672e6f642c583562a7beb987284
Rosie Wood <rwood@turing.ac.uk>
2023-04-20 16:24:38 +0100

    updated test_loader_filepaths.py

--- tests/test_loader_filepaths.py
+++ tests/test_loader_filepaths.py
@@ -1,69 +1,82 @@
 from mapreader import loader
 import pytest
 import os
-
+import pathlib
 
 @pytest.fixture
-def create_test_dir():
-    mixed_dir_name = "test_dir"
-    png_dir_name = "png_dir"
-    if not os.path.exists(mixed_dir_name):
-        os.mkdir(mixed_dir_name)
-        files = ["file1.png", "file2.png", "file3.png", "file4.tif", "file5.csv"]
-        for file in files:
-            with open(f"{mixed_dir_name}/{file}", "w") as f:
-                pass
-        if not os.path.exists(f"{mixed_dir_name}/{png_dir_name}"):
-            os.mkdir(f"{mixed_dir_name}/{png_dir_name}")
-            files = ["file1.png", "file2.png"]
-            for file in files:
-                with open(f"{mixed_dir_name}/{png_dir_name}/{file}", "w") as f:
-                    pass
+def mixed_dir(tmp_path):
+    dir_path = tmp_path / "mixed_dir"
+    os.mkdir(dir_path)
+    files = ["file1.png", "file2.png", "file3.png", "file4.tif", "file5.csv"]
+    for file in files:
+        pathlib.Path(f"{dir_path}/{file}").touch()
+    return dir_path
 
+@pytest.fixture
+def png_dir(tmp_path):
+    dir_path = tmp_path / "png_dir"
+    os.mkdir(dir_path)
+    files = ["file1.png", "file2.png"]
+    for file in files:
+        pathlib.Path(f"{dir_path}/{file}").touch()
+    return dir_path
+
+@pytest.fixture
+def empty_dir(tmp_path):
+    dir_path = tmp_path / "empty_dir"
+    os.mkdir(dir_path)
+    return dir_path
 
 # mixed dir
 
-
-def test_mixed_dir_w_file_ext(create_test_dir):
-    my_files = loader("test_dir", file_ext="png")
+def test_mixed_dir_w_file_ext(mixed_dir):
+    my_files = loader(mixed_dir, file_ext="png")
     assert len(my_files) == 3
-    my_files = loader("test_dir/", file_ext="png")
-    assert len(my_files) == 3
-
-
-def test_mixed_dir_w_star_w_file_ext(create_test_dir):
-    my_files = loader("test_dir/*", file_ext="png")
+    my_files = loader(f"{mixed_dir}/", file_ext="png")
     assert len(my_files) == 3
 
-
-def test_mixed_dir_w_file_path(create_test_dir):
-    my_files = loader("test_dir/*png")
+def test_mixed_dir_w_star_w_file_ext(mixed_dir):
+    my_files = loader(f"{mixed_dir}/*", file_ext="png")
     assert len(my_files) == 3
 
+def test_mixed_dir_w_file_path(mixed_dir):
+    my_files = loader(f"{mixed_dir}/*png")
+    assert len(my_files) == 3
 
+def test_mixed_dir_errors(mixed_dir):
+    with pytest.raises(ValueError):
+        loader(mixed_dir)
+    with pytest.raises(ValueError):
+        loader(f"{mixed_dir}/")
+        
 # unmixed dir
 
-
-def test_unmixed_dir_no_file_ext(create_test_dir):
-    my_files = loader("test_dir/png_dir")
+def test_unmixed_dir_no_file_ext(png_dir):
+    my_files = loader(png_dir)
     assert len(my_files) == 2
-    my_files = loader("test_dir/png_dir/")
-    assert len(my_files) == 2
-
-
-def test_unmixed_dir_w_star_no_file_ext(create_test_dir):
-    my_files = loader("test_dir/png_dir/*")
+    my_files = loader(f"{png_dir}/")
     assert len(my_files) == 2
 
-
-def test_unmixed_dir_w_file_path(create_test_dir):
-    my_files = loader("test_dir/png_dir/*png")
+def test_unmixed_dir_w_star_no_file_ext(png_dir):
+    my_files = loader(f"{png_dir}/*")
     assert len(my_files) == 2
 
+def test_unmixed_dir_w_file_path(png_dir):
+    my_files = loader(f"{png_dir}/*png")
+    assert len(my_files) == 2
 
 # other test cases?
 
-
-def test_file_path_w_file_ext():
-    my_files = loader("test_dir/*png", file_ext="tif")
+def test_file_path_w_file_ext(mixed_dir):
+    my_files = loader(f"{mixed_dir}/*png", file_ext="tif")
     assert len(my_files) == 3
+
+def test_empty_dir_errors(empty_dir):
+    with pytest.raises(ValueError):
+        loader(empty_dir)
+    with pytest.raises(ValueError):
+        loader(f"{empty_dir}/")
+    with pytest.raises(ValueError):
+        loader(empty_dir, file_ext="png")
+    with pytest.raises(ValueError):
+        loader(f"{empty_dir}/*")
