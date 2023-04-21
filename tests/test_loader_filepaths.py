4167524b337a7563e56203ad79ab67c963ef8811
Rosie Wood <rwood@turing.ac.uk>
2023-04-21 10:04:02 +0100

    update tests

--- tests/test_loader_filepaths.py
+++ tests/test_loader_filepaths.py
@@ -29,54 +29,60 @@ def empty_dir(tmp_path):
 
 # mixed dir
 
-def test_mixed_dir_w_file_ext(mixed_dir):
+def test_file_ext_w_mixed_dir(mixed_dir):
     my_files = loader(mixed_dir, file_ext="png")
     assert len(my_files) == 3
     my_files = loader(f"{mixed_dir}/", file_ext="png")
     assert len(my_files) == 3
 
-def test_mixed_dir_w_star_w_file_ext(mixed_dir):
+def test_file_ext_w_mixed_file_paths(mixed_dir):
     my_files = loader(f"{mixed_dir}/*", file_ext="png")
     assert len(my_files) == 3
 
-def test_mixed_dir_w_file_path(mixed_dir):
+def test_mixed_file_path(mixed_dir):
     my_files = loader(f"{mixed_dir}/*png")
     assert len(my_files) == 3
 
-def test_mixed_dir_errors(mixed_dir):
-    with pytest.raises(ValueError):
-        loader(mixed_dir)
-    with pytest.raises(ValueError):
-        loader(f"{mixed_dir}/")
-        
 # unmixed dir
 
-def test_unmixed_dir_no_file_ext(png_dir):
+def test_unmixed_di(png_dir):
     my_files = loader(png_dir)
     assert len(my_files) == 2
     my_files = loader(f"{png_dir}/")
     assert len(my_files) == 2
 
-def test_unmixed_dir_w_star_no_file_ext(png_dir):
+def test_unmixed_file_path(png_dir):
     my_files = loader(f"{png_dir}/*")
     assert len(my_files) == 2
-
-def test_unmixed_dir_w_file_path(png_dir):
     my_files = loader(f"{png_dir}/*png")
     assert len(my_files) == 2
 
 # other test cases?
 
-def test_file_path_w_file_ext(mixed_dir):
-    my_files = loader(f"{mixed_dir}/*png", file_ext="tif")
-    assert len(my_files) == 3
+#errors 
 
+def test_multiple_file_types_errors(mixed_dir):
+    with pytest.raises(ValueError, match="multiple file types"):
+        loader(mixed_dir)
+    with pytest.raises(ValueError, match="multiple file types"):
+        loader(f"{mixed_dir}/")
+    with pytest.raises(ValueError, match="multiple file types"):
+        loader(f"{mixed_dir}/*")
+
+def test_no_files_found_errors(png_dir):
+    with pytest.raises(ValueError, match="No files found"):
+        loader(png_dir, file_ext = "tif")
+    with pytest.raises(ValueError, match="No files found"):
+        loader(f"{png_dir}/*png", file_ext = "tif")
+    with pytest.raises(ValueError, match="No files found"):
+        loader(f"{png_dir}/*tif")
+    
 def test_empty_dir_errors(empty_dir):
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="No files found"):
         loader(empty_dir)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="No files found"):
         loader(f"{empty_dir}/")
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="No files found"):
         loader(empty_dir, file_ext="png")
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="No files found"):
         loader(f"{empty_dir}/*")
