c10055476436fb2676a9cc9ea5702bc6430a674d
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2023-06-06 15:30:11 +0100

    changes versioneer style

--- mapreader/_version.py
+++ mapreader/_version.py
@@ -42,7 +42,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "mapreader-"
     cfg.versionfile_source = "mapreader/_version.py"
