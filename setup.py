906e77bb19183d67b50e3847352602ed814bd769
Rosie Wood <rwood@turing.ac.uk>
2024-04-02 08:35:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     platforms="OS Independent",
-    python_requires=">=3.7, <3.11",
+    python_requires=">=3.8, <3.11",
     install_requires=[
         "matplotlib>=3.5.0,<4.0.0",
         "numpy>=1.21.5,<2.0.0",
