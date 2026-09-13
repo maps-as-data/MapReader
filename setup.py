2701c92712b9fdccad9231dc0e4d45c7c1ba02d3
Rosie Wood <rwood@turing.ac.uk>
2026-09-13 06:29:41 +0100

    update min geopandas

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setuptools.setup(
         "tqdm<5.0.0",
         "torchinfo<2.0.0",
         "openpyxl>=3.1.2,<4.0.0",
-        "geopandas<2.0.0",
+        "geopandas>=1.0.0,<2.0.0",
         "pyogrio>=0.7.2",
         "cartopy>=0.22.0",
         "joblib>=1.4.0",
